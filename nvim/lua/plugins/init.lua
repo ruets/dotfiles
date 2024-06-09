f77d771779ae403faf84b2a59d1e6719e2928947
ruets <dev@ruets.pro>
2024-06-09 17:49:25 +0200

    add copilot to nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -7,6 +7,44 @@ return {
     end,
   },
 
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    opts = {
+      -- Possible configurable fields can be found on:
+      -- https://github.com/zbirenbaum/copilot.lua#setup-and-configuration
+    suggestion = {
+        enable = false,
+      },
+      panel = {
+        enable = false,
+      },
+    },
+    lazy = false,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+          require("copilot_cmp").setup()
+        end,
+      },
+    },
+    opts = {
+      sources = {
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "copilot",  group_index = 2 },
+        { name = "luasnip",  group_index = 2 },
+        { name = "buffer",   group_index = 2 },
+        { name = "nvim_lua", group_index = 2 },
+        { name = "path",     group_index = 2 },
+      },
+    },
+  },
+
   -- These are some examples, uncomment them if you want to see them work!
   -- {
   --   "neovim/nvim-lspconfig",

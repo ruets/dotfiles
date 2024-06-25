961972bd7582fba1369191a327b3abe30921a807
ruets <dev@ruets.pro>
2024-06-25 14:01:13 +0200

    change nvim distro to lazyvim

---
+++ nvim/lua/plugins/do/init.lua
@@ -0,0 +1,26 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+if false then return {} end
+
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  {
+    "nocksock/do.nvim",
+    opts = {
+      -- default options
+      message_timeout = 2000, -- how long notifications are shown
+      kaomoji_mode = 0, -- 0 kaomoji everywhere, 1 skip kaomoji in doing
+      winbar = true,
+      doing_prefix = "Doing: ",
+      store = {
+        auto_create_file = true, -- automatically create a .do_tasks when calling :Do
+        file_name = ".do_tasks",
+      },
+    },
+  },
+}

08aef2f2a7337ecb0ac5e6ea2d17720090feddf5
ruets <dev@ruets.pro>
2024-07-12 11:52:12 +0200

    add two nvim plugins for competitive programming

---
+++ nvim/lua/plugins/competitive_programming/init.lua
@@ -0,0 +1,36 @@
+-- if true, don't actually load anything here and return an empty spec
+-- stylua: ignore
+local disabled = false
+if disabled then return {} end 
+
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  {
+    'xeluxee/competitest.nvim',
+    dependencies = 'MunifTanjim/nui.nvim',
+    config = function() require('competitest').setup() end,
+  },
+
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim", -- required by telescope
+        "MunifTanjim/nui.nvim",
+
+        -- optional
+        "nvim-treesitter/nvim-treesitter",
+        "rcarriga/nvim-notify",
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+        -- configuration goes here
+    },
+  },
+}

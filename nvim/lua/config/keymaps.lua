e1d8d79393287c6099ea021bb977d769eea4dc37
ruets <dev@ruets.pro>
2024-07-18 10:48:10 +0200

    add new cmp keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,6 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.del("n", "<leader>l")
+
 local wk = require("which-key")
 wk.register({
   ["<leader>m"] = { name = "+multiple cursors" },
@@ -10,3 +12,17 @@ wk.register({
 local map = LazyVim.safe_keymap_set
 map("n", "<leader>ll", "<cmd>Lazy<cr>", { desc = "Lazy" })
 map("n", "<leader>lc", LazyVim.pick.config_files(), { desc = "Config" })
+
+-- Assuming you have 'hrsh7th/nvim-cmp' installed
+local cmp = require'cmp'
+
+cmp.setup({
+  mapping = {
+    ['<CR>'] = cmp.config.disable,
+    ['<Down>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'}),
+    ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'}),
+    ['<Tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item
+    ['<A-c>'] = cmp.mapping.complete(),
+  },
+})
+

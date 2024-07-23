952aca63b9c627b9b1a873bb20f7dbe0cfb3ce06
ruets <dev@ruets.pro>
2024-07-23 09:39:06 +0200

    add several plugins and refactor plugins dir

---
+++ nvim/lua/plugins/ui/init.lua
@@ -0,0 +1,21 @@
+-- if true, don't actually load anything here and return an empty spec
+-- stylua: ignore
+local disabled = false
+if disabled then return {} end 
+
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }
+}

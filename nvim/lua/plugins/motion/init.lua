5c20ed87420c86813c971152f2b1b9d0bf290fdb
ruets <dev@ruets.pro>
2024-07-23 13:28:23 +0200

    fix a bug on precognition config

--- nvim/lua/plugins/motion/init.lua
+++ nvim/lua/plugins/motion/init.lua
@@ -10,33 +10,35 @@ if disabled then return {} end
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
-  "tris203/precognition.nvim",
-  --event = "VeryLazy",
-  opts = {
-  -- startVisible = true,
-  -- showBlankVirtLine = true,
-  -- highlightColor = { link = "Comment" },
-  -- hints = {
-  --      Caret = { text = "^", prio = 2 },
-  --      Dollar = { text = "$", prio = 1 },
-  --      MatchingPair = { text = "%", prio = 5 },
-  --      Zero = { text = "0", prio = 1 },
-  --      w = { text = "w", prio = 10 },
-  --      b = { text = "b", prio = 9 },
-  --      e = { text = "e", prio = 8 },
-  --      W = { text = "W", prio = 7 },
-  --      B = { text = "B", prio = 6 },
-  --      E = { text = "E", prio = 5 },
-  -- },
-  -- gutterHints = {
-  --     G = { text = "G", prio = 10 },
-  --     gg = { text = "gg", prio = 9 },
-  --     PrevParagraph = { text = "{", prio = 8 },
-  --     NextParagraph = { text = "}", prio = 8 },
-  -- },
-  -- disabled_fts = {
-  --     "startify",
-  -- },
+  {
+    "tris203/precognition.nvim",
+    -- event = "VeryLazy",
+    opts = {
+      -- startVisible = true,
+      -- showBlankVirtLine = true,
+      -- highlightColor = { link = "Comment" },
+      -- hints = {
+      --   Caret = { text = "^", prio = 2 },
+      --   Dollar = { text = "$", prio = 1 },
+      --   MatchingPair = { text = "%", prio = 5 },
+      --   Zero = { text = "0", prio = 1 },
+      --   w = { text = "w", prio = 10 },
+      --   b = { text = "b", prio = 9 },
+      --   e = { text = "e", prio = 8 },
+      --   W = { text = "W", prio = 7 },
+      --   B = { text = "B", prio = 6 },
+      --   E = { text = "E", prio = 5 },
+      -- },
+      -- gutterHints = {
+      --   G = { text = "G", prio = 10 },
+      --   gg = { text = "gg", prio = 9 },
+      --   PrevParagraph = { text = "{", prio = 8 },
+      --   NextParagraph = { text = "}", prio = 8 },
+      -- },
+      -- disabled_fts = {
+      --   "startify",
+      -- },
+    },
   },
   {
       'smoka7/hop.nvim',

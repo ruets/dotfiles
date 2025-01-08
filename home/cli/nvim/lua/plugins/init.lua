bb8cf7e22e767d0d388b5c8374a82113ea05dd18
ruets <dev@ruets.pro>
2025-01-08 23:30:19 +0100

    nvim:disable what's new window at startup

--- home/cli/nvim/lua/plugins/init.lua
+++ home/cli/nvim/lua/plugins/init.lua
@@ -1,1 +1,14 @@
-return {}
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      news = {
+        -- When enabled, NEWS.md will be shown when changed.
+        -- This only contains big new features and breaking changes.
+        lazyvim = false,
+        -- Same but for Neovim's news.txt
+        neovim = false,
+      },
+    },
+  },
+}

bec4b610c2745c2eb170dfd5387cf24c72cf0ee0
ruets <dev@ruets.pro>
2025-04-16 17:16:10 +0200

    texlive:add full texlive distro

--- home/languages/texlive.nix
+++ home/languages/texlive.nix
@@ -1,7 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  programs = {
-    texlive.enable = true; 
-  };
+  # programs = {
+  #   texlive.enable = true; 
+  # };
+
+  home.packages = with pkgs; [
+    texlive.combined.scheme-full
+  ];
 }

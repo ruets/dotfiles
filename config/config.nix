b0cb8aa2e74366159358ae31b1a31c38e05a13af
ruets <dev@ruets.pro>
2025-10-27 13:36:21 +0100

    add nixgl for gui compatibility and add username to config spec

--- config/config.nix
+++ config/config.nix
@@ -1,18 +1,11 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
-let
-  user = let
-    envUser = builtins.getEnv "USER";
-  in
-    if envUser == "" then "ruets" else envUser;
-in{
+{
   programs.home-manager.enable = true;
   nixpkgs.config.allowUnfree = true;
 
   home = {
     stateVersion = "24.11";
-    username = user;
-    homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${user}" else "/home/${user}";
     language.base = "fr_FR.UTF-8";
   };
 
@@ -23,4 +16,8 @@ in{
       experimental-features = [ "nix-command" "flakes" ];
     };
   };
+
+  nixGL.packages = inputs.nixgl.packages;
+  nixGL.defaultWrapper = "mesa";
+  nixGL.installScripts = [ "mesa" ];
 }

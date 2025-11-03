ce6c47e4cf3174621f28a38cfbacab92432576ec
ruets <dev@ruets.pro>
2025-11-03 07:03:27 +0100

    add some fish scripts and some fixes

--- home/cli/fish/functions/dev.fish
+++ home/cli/fish/functions/dev.fish
@@ -1,4 +1,4 @@
-function vite
+function dev
     if test (count $argv) -eq 0
         set dir (pwd)
     else
@@ -13,6 +13,10 @@ function vite
         tmux new-session -d -s $dirname -n nvim
         tmux send-keys -t $dirname:nvim nvim C-m
 
+        # New window for AI
+        tmux new-window -t $dirname -n AI
+        tmux send-keys -t $dirname:AI $AI C-m
+
         # New window for lazygit
         tmux new-window -t $dirname -n lazygit
         tmux send-keys -t $dirname:lazygit lazygit C-m
@@ -20,10 +24,6 @@ function vite
         # New window for fish
         tmux new-window -t $dirname -n fish
         tmux send-keys -t $dirname:fish lt C-m
-
-        # New window for nodejs
-        tmux new-window -t $dirname -n nodejs
-        tmux send-keys -t $dirname:nodejs 'npm run dev' C-m
     end
     tmux attach -t $dirname:nvim
 end

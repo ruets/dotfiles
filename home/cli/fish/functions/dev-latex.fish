eb64e945904f27770d5d94e7982c776544faa46a
ruets <dev@ruets.pro>
2025-11-03 07:03:27 +0100

    add some fish scripts and some fixes

--- home/cli/fish/functions/dev-latex.fish
+++ home/cli/fish/functions/dev-latex.fish
@@ -1,4 +1,4 @@
-function script_latex
+function dev-latex
     set -l WORKING_DIR $argv[1]
     if test -z "$WORKING_DIR"
         echo "Usage: latex <working_dir>"
@@ -12,6 +12,14 @@ function script_latex
         tmux new-session -d -s latex_$PWD -n nvim
         tmux send-keys -t latex_$PWD:nvim nvim C-m
         tmux send-keys -t latex_$PWD:nvim ":NvimTreeFocus" C-m
+
+        # New window for AI
+        tmux new-window -t $dirname -n AI
+        tmux send-keys -t $dirname:AI $AI C-m
+
+        # New window for fish
+        tmux new-window -t $dirname -n fish
+        tmux send-keys -t $dirname:fish lt C-m
 
         # New window for zathura
         tmux new-window -t latex_$PWD -n zathura

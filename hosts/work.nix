a59d6634a20e4b942ea2457f967d101ec3ba9feb
ruets <dev@ruets.pro>
2025-11-10 09:50:28 +0100

    remove some packages from work, and remove nixgl from non necessary

--- hosts/work.nix
+++ hosts/work.nix
@@ -10,17 +10,11 @@
 
     ../home/softwares/1password/1password.nix
     ../home/softwares/cava/cava.nix
-    ../home/softwares/discord/discord.nix
     ../home/softwares/docker/docker.nix
-    ../home/softwares/google-chrome/google-chrome.nix
     ../home/softwares/kitty/kitty.nix
     ../home/softwares/mission-center/mission-center.nix
-    ../home/softwares/qbittorrent/qbittorrent.nix
-    ../home/softwares/qdirstat/qdirstat.nix
     ../home/softwares/qualculate/qalculate.nix
-    ../home/softwares/scrcpy/scrcpy.nix
     ../home/softwares/spotify/spotify.nix
-    ../home/softwares/waydroid/waydroid.nix
     ../home/softwares/zathura/zathura.nix
 
     ../home/languages/node.nix

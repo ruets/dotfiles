4b60b7a9fad94bdab285edd6ffe024da11d74abc
ruets <dev@ruets.pro>
2026-02-03 10:11:05 +0100

    add backup scripts

--- home/modules/vps.nix
+++ home/modules/vps.nix
@@ -1,17 +1,42 @@
 { config, pkgs, ... }:
 
 {
-  home.file = {
-    "services/scripts" = {
-      source = ./services/scripts;
-      recursive = true;
-      force = true;
+  home = {
+    packages = with pkgs; [
+      restic
+    ];
+
+    file = {
+      "services/scripts" = {
+        source = ./vps/scripts;
+        recursive = true;
+        force = true;
+      };
+
+      "services/configs".source = ./vps/configs;
+
+      "services/docker-compose.yml".source = ./vps/docker-compose.yml;
+  
+      "services/secrets/.keep".text = "";
     };
+  };
 
-    "services/configs".source = ./vps/configs;
+  # Systemd user service
+  systemd.user.services.backup = {
+    description = "Restic Backup Wasabi";
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "~/services/scripts/backup.sh";
+    };
+  };
 
-    "services/docker-compose.yml".source = ./vps/docker-compose.yml;
- 
-    "services/secrets/.keep".text = "";
+  # Systemd timer pour automatiser tous les jours à 2h
+  systemd.user.timers.backup = {
+    description = "Run backup daily at 2am";
+    timerConfig = {
+      OnCalendar = "*-*-* 02:00:00";
+      Persistent = true;
+    };
+    wantedBy = [ "timers.target" ];
   };
 }

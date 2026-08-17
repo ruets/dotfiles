7bf863d1464055b0043053e83f7d447d240be94b
Sébastien RUET <sebastien.ruet@insa-lyon.fr>
2026-08-17 16:37:01 +0200

    add claude code and chezit, fix rdp fish function, change fullscreen opacity for hyprland

--- dot_config/fish/functions/rdp.fish
+++ dot_config/fish/functions/rdp.fish
@@ -3,5 +3,5 @@ function rdp
     read -P "User : " user
     read -s -P "Mot de passe : " pass
     echo
-    xfreerdp /grab-keyboard /v:$ip /u:$user /p:$pass /size:100% /dynamic-resolution /gfx:AVC444,progressive:on
+    xfreerdp /grab-keyboard /v:$ip /u:$user /p:$pass /size:100% /dynamic-resolution /gfx:progressive:on
 end
